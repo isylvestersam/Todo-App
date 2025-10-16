1422c49e64a704cdc734b3eeddfad2c363b02e16
isylsam <isylvestersam@gmail.com>
2025-10-16 21:16:38 +0100

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'Jose' : ['Josefin Sans']
+      }
+    },
+  },
+  plugins: [],
+}
+
